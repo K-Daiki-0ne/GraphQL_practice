ef4ef5ee98c7b760a465228c45c7719f570198a2
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-03-11 07:05:30 +0900

    create: About page

---
+++ app/src/pages/about.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { NextPage } from 'next';
+import { Header } from '@src/components/molecules'
+import styles from '../styles/Home.module.scss'
+
+const About:NextPage = (): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next.js GraphQL practice</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main className={styles.main}>
+        <p>About page</p>
+      </main>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default About
