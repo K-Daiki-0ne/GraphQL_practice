aa540965cdc6e6601f1eb3c6d18bbee6105ea8ac
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-03-11 07:12:44 +0900

    create: Imte & Post component

---
+++ app/src/components/organisms/Item/Item.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const Item:FC = (): JSX.Element => {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ app/src/components/templates/Post/Post.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+export const Post:FC = (): JSX.Element => {
+  return (
+    <div>
+
+    </div>
+  )
+}
