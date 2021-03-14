a69fbefb2c25e7d4cbfc660cbdd9db39cd3fa480
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-03-14 22:58:18 +0900

    modified: Header design

--- app/src/components/molecules/Header/Header.tsx
+++ app/src/components/molecules/Header/Header.tsx
@@ -4,13 +4,18 @@ import styles from './Header.module.scss';
 
 export const Header: FC = (): JSX.Element => {
   return (
-    <header className={styles.sample}>
-      <Link href='/'>
-        <p>Home</p>
-      </Link>
-      <Link href='/about'>
-        <p>About</p>
-      </Link>
-    </header>
+    <div className={styles.header}>
+      <header className={styles.content}>
+        <h1 className={styles.title}>GraphQL practice with NextJS</h1>
+        <nav className={styles.navagation}>
+          <Link href='/' >
+            <a className={styles.linkPage}>Home</a>
+          </Link>
+          <Link href='/about'>
+            <a className={styles.linkPage}>About</a>
+          </Link>
+        </nav>
+      </header>
+    </div>
   )
 }
