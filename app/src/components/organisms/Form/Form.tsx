dd22e6be84372737f8feaf04d139ee1af28fc717
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-03-14 23:49:24 +0900

    create: Post data form

--- app/src/components/organisms/Form/Form.tsx
+++ app/src/components/organisms/Form/Form.tsx
@@ -1,9 +1,30 @@
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import styles from './Form.module.scss';
+
+interface FormData {
+  title: string;
+  body: string;
+}
 
 export const Form: FC = (): JSX.Element => {
+  const { register, handleSubmit } = useForm<FormData>()
+
+  const onSubmit = handleSubmit(({ title, body }) => {
+    console.log(title, body)
+  })
+
   return (
-    <div>
-      <p>POST</p>
+    <div className={styles.form}>
+      <h1 className={styles.title} >Post Data Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.formContent}>
+          <input type="text" name="title" ref={register} className={styles.title}  placeholder='title' />
+          <textarea name="body" ref={register} className={styles.body} placeholder='content' />
+          
+          <input type="submit" className='submit' />
+        </div>
+      </form>
     </div>
   )
 }
